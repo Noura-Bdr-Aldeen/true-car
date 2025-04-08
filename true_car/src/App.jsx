3eb992e08ef349eb2d359d3031ad4e541da7e6f3
Noura-Bdr-Aldeen <nourabdraldeen@gmail.com>
2025-04-08 14:31:33 +0300

    true car

---
+++ true_car/src/App.jsx
@@ -0,0 +1,22 @@
+
+// import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from "../src/pages/Home"
+import CarDetails from "../src/pages/CarDetails"
+import Footer from './components/footer/Footer';
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+      </Routes>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ true_car/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="blue">facebook</p>
+      <a href="">insta</a>
+    </div>
+  )
+}
+
+export default Footer
