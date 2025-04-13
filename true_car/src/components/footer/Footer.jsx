1381565a52bbda2c2c468577f5669b10b768a909
Noura-Bdr-Aldeen <nourabdraldeen@gmail.com>
2025-04-13 11:16:21 +0300

    footer

--- true_car/src/components/footer/Footer.jsx
+++ true_car/src/components/footer/Footer.jsx
@@ -1,12 +1,47 @@
-import "./footer.css"
+import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Facebook,
+  Twitter,
+  Google,
+  Instagram,
+  Linkedin,
+  Github
+} from 'react-bootstrap-icons';
 
 const Footer = () => {
   return (
-    <div>
-      <p className="blue">facebook</p>
-      <a href="">insta</a>
-    </div>
-  )
-}
+    <footer className="bg-light text-muted px-5 mx-5">
+      <Container fluid className="border-bottom">
+        <Row className="py-4 d-flex align-items-center">
+          <Col md={6} className="text-center text-md-start mb-3 mb-md-0">
+            <div>
+              © {new Date().getFullYear()} Copyright:
+              <a className="text-reset fw-bold ms-1 text-decoration-none" href="#">
+                TrueCar.com
+              </a>
+            </div>
+          </Col>
+          <Col md={6} className="text-center text-md-end">
+            <a href="#" className="me-4 text-reset">
+              <Facebook className="fs-5" />
+            </a>
+            <a href="#" className="me-4 text-reset">
+              <Twitter className="fs-5" />
+            </a>
+            <a href="#" className="me-4 text-reset">
+              <Google className="fs-5" />
+            </a>
+            <a href="#" className="me-4 text-reset">
+              <Instagram className="fs-5" />
+            </a>
+            <a href="#" className="me-4 text-reset">
+              <Linkedin className="fs-5" />
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
