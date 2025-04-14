475ea815bbc98c3ccd079db38c7c961fedac3458
Noura-Bdr-Aldeen <nourabdraldeen@gmail.com>
2025-04-14 09:38:40 +0300

    initial

--- true_car/src/main.jsx
+++ true_car/src/main.jsx
@@ -5,11 +5,14 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter } from 'react-router-dom';
+import ReactQueryProvider from './providers/QueryClientProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ReactQueryProvider>
+        <App />
+      </ReactQueryProvider>
     </BrowserRouter>
   </StrictMode>,
 )
